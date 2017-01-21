dc832e4ea3eb9560689c615081358551df64f5ba
VEBER Arnaud <arnaud@veber.pw>
2017-01-21 11:19:54 +0100

    feat(bundle): create kong admin bundle

---
+++ src/KongAdminBundle.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the KongAdminBundle package.
+ *
+ * (c) Unikorp <https://github.com/unikorp>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Unikorp\KongAdminBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * kong admin bundle
+ *
+ * @author VEBER Arnaud <https://github.com/VEBERArnaud>
+ */
+class KongAdminBundle extends Bundle
+{
+}
